aa32b91f12bf8096ad354786b2796768814e2a7b
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-03-21 10:21:39 -0300

    style: increase gutterWidth to 30px

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -95,7 +95,7 @@ export default {
     sideBarWidth: 375,
     pdfPageMaxWidth: 992,
     tooltipzIndex: 10050,
-    gutterWidth: 12,
+    gutterWidth: 30,
     popoverMenuShadow: '0px 4px 12px 0px rgba(0, 0, 0, 0.06)',
     optionRowHeight: 64,
     optionRowHeightCompact: 52,
